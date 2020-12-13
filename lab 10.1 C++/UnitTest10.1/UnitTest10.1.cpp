7e65a8c10815d251a3f71970e6c4de038817a880
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-12-13 22:32:52 +0200

    lab 10.1 c++

---
+++ lab 10.1 C++/UnitTest10.1/UnitTest10.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 10.1 C++/lab 10.1 C++.cpp"
+#define TESTING
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest101
+
+{
+	TEST_CLASS(UnitTest101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			bool functionResult = find();
+
+			Assert::AreEqual(false, functionResult);
+		}
+	};
+}
